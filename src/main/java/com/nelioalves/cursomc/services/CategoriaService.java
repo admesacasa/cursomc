569556d3c4ddf0c14a9fa1cd34aaa76109f40506
Adriano Mesacasa <adrianomesacasa@gmail.com>
2018-07-13 17:21:56 -0300

    Criado juste endepoint categorias/{id}

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -2,11 +2,13 @@ package com.nelioalves.cursomc.services;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService{
@@ -15,7 +17,9 @@ public class CategoriaService{
 	private CategoriaRepository repo;
 	
 	public Categoria busca(Integer id) {
-		Optional<Categoria> categoria  = repo.findById(id);
-		return categoria.orElse(null);
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+
 	}
 }
