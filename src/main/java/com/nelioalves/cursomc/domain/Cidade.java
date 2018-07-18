3fa663e4a2c661ec53dc6db0f38e6ed34ee0bc06
Adriano Mesacasa <adrianomesacasa@gmail.com>
2018-07-18 17:20:17 -0300

    Criado cliente, endereço e telefone

--- src/main/java/com/nelioalves/cursomc/domain/Cidade.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cidade.java
@@ -1,5 +1,7 @@
 package com.nelioalves.cursomc.domain;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
-public class Cidade {
+public class Cidade implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@Id
